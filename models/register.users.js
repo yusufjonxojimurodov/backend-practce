4c0b62a40430c4f8d6debdaefd6fddba08065d38
Abror305 <dilfuzaalmatova78@gmail.com>
2025-09-08 00:03:35 +0500

    ASD

--- models/register.users.js
+++ models/register.users.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
+  import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName:  { type: String, required: true },
-  email:     { type: String, required: true, unique: true },
-  password:  { type: String, required: true },
-  role: {
-    type: String,
-    enum: ["customer", "seller", "admin"],
-    default: "customer"
-  }
-}, { timestamps: true });
+  const userSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName:  { type: String, required: true },
+    email:     { type: String, required: true, unique: true },
+    password:  { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["customer", "seller", "admin"],
+      default: "customer"
+    }
+  }, { timestamps: true });
 
-const User = mongoose.model("User", userSchema);
-export default User;
+  const User = mongoose.model("User", userSchema);
+  export default User;
